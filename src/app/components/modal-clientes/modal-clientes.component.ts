7ab7c5653db90dcd247bba0a97c163da00c72bc5
Domingo Pineda <domingoantonio2072@gmail.com>
2021-06-03 21:30:02 -0400

    Creacion del formulario clientes

--- src/app/components/modal-clientes/modal-clientes.component.ts
+++ src/app/components/modal-clientes/modal-clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-clientes',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-clientes.component.css']
 })
 export class ModalClientesComponent implements OnInit {
-
-  constructor() { }
+ formClientes!: FormGroup;
+ submitted: boolean = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.formClientes = this.formBuilder.group({
+      idCliente:[0],
+      nombres:[null,[Validators.required,Validators.maxLength(50)]],
+      apellidos:[null,[Validators.required,Validators.maxLength(50)]],
+      cedula:[null,[Validators.required,Validators.minLength(11),Validators.maxLength(11),Validators.pattern('[0-9]*')]]
+    }); }
 
+    get f() {
+      return this.formClientes.controls;
+    }
+
+    onSubmit(): void {
+
+    }
 }
+
+
